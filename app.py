dffbb4235e29ea77549d76b11bb6de57adc3fc09
Eudess <jadieleudes@gmail.com>
2023-09-02 14:43:36 -0300

    refactore: update query_process_number

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, render_template, request
+from utils import utils
 
 app = Flask(__name__)
 
@@ -8,8 +9,9 @@ def home():
 
 @app.route('/numero_processo', methods=['post'])
 def query_process_number():
-    numero_processo = request.form.get('numero_processo')
-    print(numero_processo)
+    process_number = request.form.get('numero_processo')
+    formatted_process_number = utils.formatter_npu(process_number)
+
     return render_template('process.html')
 
 if __name__ == '__main__':
