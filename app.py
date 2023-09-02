11694787575cd2ca2abd601c8c67846dbc87e459
Eudess <jadieleudes@gmail.com>
2023-09-02 13:43:54 -0300

    feat: add route for process number

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/numero_processo', methods=['post'])
+def query_process_number():
+    numero_processo = request.form.get('numero_processo')
+    print(numero_processo)
+    return render_template('process.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
