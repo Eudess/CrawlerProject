b8110a10dbcbc182afe156f212dc2cb6794dfe9c
Eudess <jadieleudes@gmail.com>
2023-09-05 20:40:23 -0300

    feat: create xpath for partes

--- crawler/crawler/spiders/base/constants/extract.py
+++ crawler/crawler/spiders/base/constants/extract.py
@@ -6,6 +6,12 @@ HEADER_XPATH = {
     "juiz": "//td[contains(@class, 'descricaoMovimentacao')]/span[contains(normalize-space(), 'Nome do juiz')][1]/text()"
 }
 
+PARTES_XPATH = {
+    "table": "//table[contains(@id, 'tableTodasPartes')]//tr",
+    "papel": "./td/span/text()",
+    "nome": "./td[contains(@class, 'nomeParteEAdvogado')]/text()"
+}
+
 REGEX = {
     "distri": r"\d+\/\d+\/\d+",
     "juiz": r"Nome do juiz\(a\): (.+)\sNome"
