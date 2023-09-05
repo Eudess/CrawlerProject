a6650527a3153eef46b5c566cb0df747e9b9aa46
Eudess <jadieleudes@gmail.com>
2023-09-04 21:09:18 -0300

    feat: exec spider in app

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, render_template, request
 from utils import utils
 
@@ -11,8 +12,15 @@ def home():
 def query_process_number():
     process_number = request.form.get('numero_processo')
     formatted_process_number = utils.formatter_npu(process_number)
+    tribunal = utils.get_tribunal(formatted_process_number)
+    if tribunal == "tjce":
+        spider_logs = start_spider_ce(formatted_process_number)
+        return render_template('process.html', spider_logs=spider_logs)
 
     return render_template('process.html')
+
+def start_spider_ce(process_number):
+   os.system("cd crawler && scrapy crawl esaj_tjce -a numero_processo=" + process_number)
 
 if __name__ == '__main__':
     app.run(debug=True)
