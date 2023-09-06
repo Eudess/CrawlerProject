1abe1684394e2494d8305a8b97d421643714d2be
Eudess <jadieleudes@gmail.com>
2023-09-05 21:29:58 -0300

    feat: create extract_movimentacoes

--- crawler/crawler/spiders/base/spider.py
+++ crawler/crawler/spiders/base/spider.py
@@ -44,27 +44,35 @@ class SpiderBase(scrapy.Spider):
         item['data_distribuicao'] = data_distribuicao
         item['juiz'] = juiz
 
-        yield from self.extract_partes(response)
-        yield from self.extrat_andamentos(response)
         yield item
+        yield from self.extract_partes(response)
+        yield from self.extrat_movimentacoes(response)
 
     def extract_partes(self, response):
         partes = response.xpath(PARTES_XPATH["table"])
         for parte in partes:
-            papel = parte.xpath(PARTES_XPATH["papel"]).get()
-            nome = parte.xpath(PARTES_XPATH["nome"]).get()
+            papel = parte.xpath(PARTES_XPATH["papel"]).get().strip()
+            nome = parte.xpath(PARTES_XPATH["nome"]).get().strip()
 
             item = PartesItem()
-            item['nome'] = nome.strip()
-            item['papel'] = papel.strip()
-
+            item['nome'] = nome
+            item['papel'] = papel
 
             yield item
 
             
-    def extrat_andamentos(self, response):
-        pass
+    def extrat_movimentacoes(self, response):
+        movimentacoes = response.xpath(MOVIMENTACOES_XPATH["table"])
+        for movimentacao in movimentacoes:
+            data = movimentacao.xpath(MOVIMENTACOES_XPATH["data"]).get().strip()
+            texto = movimentacao.xpath(MOVIMENTACOES_XPATH["texto"]).get().strip()
 
+            if texto:
+                item = MovimentacoesItem()
+                item['data'] = data
+                item['texto'] = texto
+
+                yield item
         
     def get_numero_ano_unificado(self, process_number):
         return f"{process_number[:15]}"
