171ba8462c13a0d4e8aa6cf4f4c55c9aeb929640
Eudess <jadieleudes@gmail.com>
2023-09-07 12:37:08 -0300

    feat: add tjal 1 grau in app.py

--- app.py
+++ app.py
@@ -19,11 +19,19 @@ def query_process_number():
         spider_logs = start_spider_ce(formatted_process_number)
         process_files = read_process_files(formatted_process_number)
         return render_template('process.html', spider_logs=spider_logs, process_data=process_files[0])
+    elif tribunal == "tjal":
+        spider_logs = start_spider_al(formatted_process_number)
+        process_files = read_process_files(formatted_process_number)
+        return render_template('process.html', spider_logs=spider_logs, process_data=process_files[0])
 
     return render_template('process.html')
 
 def start_spider_ce(process_number):
     cmd = f"cd crawler && scrapy crawl esaj_tjce-1-grau -a numero_processo={process_number}"
+    subprocess.run(cmd, shell=True, check=True, text=True)
+
+def start_spider_al(process_number):
+    cmd = f"cd crawler && scrapy crawl esaj_tjal-1-grau -a numero_processo={process_number}"
     subprocess.run(cmd, shell=True, check=True, text=True)
 
 
